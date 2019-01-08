c6dd2848fe44c661419732ab63990837d714616e
zhangyunkang <1124773718@qq.com>
2019-01-08 21:59:47 +0800

    解决oss上传问题

--- mall-admin-web/config/index.js
+++ mall-admin-web/config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/mall/images': {
+        target: 'http://zykbucket.oss-cn-beijing.aliyuncs.com',  // 接口域名
+        changeOrigin: true,  //是否跨域
+        pathRewrite: {
+          '^/mall/images': ''   //需要rewrite重写的,
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
